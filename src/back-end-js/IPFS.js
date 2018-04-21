360d360af41c39a034f440b1f782e9707c0b6e71
Fredric Moezinia <moezinia@mit.edu>
2018-04-21 17:09:56 -0400

    fixed ipfs problems

--- src/back-end-js/IPFS.js
+++ src/back-end-js/IPFS.js
@@ -4,84 +4,53 @@ const node = new IPFS();
 const series = require('async/series');
 
 
+
 function sendToIPFS(dataToSend) {
-    console.log("data to send is from IPFS.js! ", dataToSend);
-
-    var fileMultihash;
-    series([
-      (cb) => node.on('ready', cb),
-      (cb) => node.version((err, version) => {
-        if (err) {
-            console.log("error version");
-             return cb(err)
-          }
-        console.log('Version:', version.version)
-        cb()
-      }),
-      // Create the File to add, a file consists of a path + content. More details on
-
-      (cb) => node.files.add({
-        path: 'hello.txt',
-        content: Buffer.from(dataToSend)
-      }, (err, filesAdded) => {
-        if (err) {
-        console.log("error writing to file");
-        return cb(err);
-        }
-        console.log('\nAdded file:', filesAdded[0].path, filesAdded[0].hash)
-        fileMultihash = filesAdded[0].hash
-        cb()
-      }),
-      // RETRIEVE files from ipfs
-      (cb) => node.files.cat(fileMultihash, (err, data) => {
-        if (err) {
-        console.log("err in catting");
-        return cb(err)
-        }
-
-        console.log('\nFile content:')
-        process.stdout.write(data)
-    },
-    console.log("hi2")
-),
-
-],
-    function(err, result) {
-        if (err) { return "error" }
-        return "success";
-        console.log(fileMultihash, 'si');
+  node.files.add({
+    path: 'hello.txt',
+    content: Buffer.from(JSON.stringify(dataToSend))
+  }, (err, filesAdded) => {
+    if (err) {
+      console.log("error writing to file");
+      return err;
     }
+  else {
+    fileHash = filesAdded[0].hash
+    console.log('\nAdded file:', filesAdded);
+    // just read out contents if need!
+    node.files.cat(fileHash, (err, data) => {
+      if (err) {
+      console.log("err in catting");
+      return err
+      }
+      console.log('\nFile content:')
+      process.stdout.write(data)
+    })
+  }
+  }
 )
 }
 
-
-
-
 function getFromIPFS(file) {
+  // fileHash like '/ipfs/QmXEmhrMpbVvTh61FNAxP9nU7ygVtyvZA8HZDUaqQCAb66/a.txt'
 
-    series([
-        (cb) => node.on('ready', cb),
+  node.files.get(fileHash, (err, data) => {
+    if (err) {
+      console.log("error", err);
+    }
+    else {
 
-        // RETRIEVE files from ipfs
-        // fileMultihash like '/ipfs/QmXEmhrMpbVvTh61FNAxP9nU7ygVtyvZA8HZDUaqQCAb66/a.txt'
-        (cb) => node.files.get(fileMultihash, (err, data) => {
-          if (err) {
-          console.log("err in catting");
-          return cb(err)
-          }
+  //           data = array of {
+  //   path: '/tmp/myfile.txt',
+  //   content: <data as a Buffer>
+  // }
+  console.log(data.content.toString('utf8'));
+  // TODO POST TO front end?
+  process.stdout.write(data)
 
-          console.log('\nFile content:')
+    }
+  })
 
-        //           data = array of {
-        //   path: '/tmp/myfile.txt',
-        //   content: <data as a Buffer>
-        // }
-
-        console.log(data.content.toString('utf8'));
-          // TODO POST TO front end?
-          process.stdout.write(data)
-      })
-  ])
 }
 
 module.exports = {sendToIPFS: sendToIPFS, getFromIPFS: getFromIPFS};
