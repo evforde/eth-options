a92e7e397c40391cb1678f1b736a47838ec0c007
Fredric Moezinia <moezinia@mit.edu>
2018-04-25 10:20:53 -0400

    most contract deployment code for options

--- public/front-end-js/create-option.js
+++ public/front-end-js/create-option.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const solc = require('solc');
-const web3 = require('web3');
 
+// already done in metamask.js ?
+// var Web3 = require('web3');
+// var web3 = new Web3();
 // Connected already, right?
 // const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
@@ -18,13 +20,16 @@ class callOption {
     this.optionValue = optionValue;
     // only allow fulfillemnt before certain datetime
     this.offerExpiry = offerExpiry;
-
-
   }
-  // // Getter
-  // get area() {
-  //   return this.calcArea();
-  // }
+
+  set setSmartContractAddress(address) {
+    this.contractAddress = address;
+  }
+
+  // Getters
+  get contractAddress() {
+    return this.contractAddress;
+  }
 }
 
 
@@ -51,8 +56,8 @@ $(document).ready(function() {
     maturityDate = $("maturity").text();
     ETHStrikePrice = $("ETHStrikePrice").text();
     premiumPrice = $("premiumPrice").text();
-    optionCreatorType = "buyer or seller";
-    optionCreatorAddress = "0x......";
+    optionCreatorType = "writer";//or holder TODO how to do this nicely in UI - create bid /create ask>?
+    optionCreatorAddress = web3.eth.contracts[0] //TODO correct? from metamask...
     offerExpiry = $("offerExpiry").text();
     optionValue = $("numberETH").text();
 
@@ -65,22 +70,127 @@ $(document).ready(function() {
       optionValue, offerExpiry)
 
 
+    const smartContractInstance = optionSmartContractOperation(curOption);
+
 
     sendToIPFS(curOption);
+
+    // update list of unfulfilled options
+
+    //update my options tab
   });
 
 });
 
 
+class optionSmartContractOperation {
 
-function instantiateOptionSmartContract(optionObj) {
-  // Compile the source code
-  const input = fs.readFileSync('Token.sol');
-  const output = solc.compile(input.toString(), 1);
-  const bytecode = output.contracts['Token'].bytecode;
-  const abi = JSON.parse(output.contracts['Token'].interface);
+  constructor(optionObj) {
+    this.optionObj = optionObj;
+    smartContractAddress = instantiateOptionSmartContract(optionObj);
+
+    if (!smartContractAddress) {
+      console.log("ERROR DEPLOYING Contract");
+    }
+    else {
+      // desposit funds
+      if (optionObj.optionCreatorType == "holder") {
+        const transactionObj = {from: optionObj.optionCreatorAddress,
+          to:smartContractAddress, value: web3.toWei(premiumPrice, 'ether'),
+          gas: 21000, gasPrice: 20000000000}
+        web3.eth.sendTransaction(transactionObj, (err, result) => {
+          if (!err) {
+            console.log(JSON.stringify(result));
+          }
+          else {
+            console.error(err);
+          }
+        });
+
+      }
+
+
+      if (optionObj.optionCreatorType == "writer") {
+
+
+
+
+
+
+
+
+
+      }
+
+
+
+
+    }
+
+  }
+
+  function desposit() {
+
+  }
+
+
+  //TODO add myContract.options.address = '0x1234..' (address of smart contract)
+
+  function instantiateOptionSmartContract(optionObj) {
+    const smartContractAddress;
+    // Compile the source code
+    const input = fs.readFileSync('Option.sol');
+    const output = solc.compile(input.toString(), 1);
+    const bytecode = output.contracts[':Option'].bytecode;
+    // abi is jsoninterface https://web3js.readthedocs.io/en/1.0/glossary.html#glossary-json-interface
+    const abi = JSON.parse(output.contracts[':Option'].interface);
+
+    const optionSmartContract = web3.eth.contract(abi); //const contract = new web3.eth.Contract(abi, '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe', {
+
+    const fallbackValues = {
+      // data: '0x' + bytecode, //TODO
+      from: optionObj.optionCreatorAddress,
+      gas: 90000*2 // TODO change
+    }
+
+    const optionSmartContractInstance = optionSmartContract.new(
+      fallbackValues, (err, res) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      // Log the tx,to use eth.getTransaction()
+      console.log(res.transactionHash);
+
+      // If we have an address property, the contract was deployed
+      if (res.address) {
+        smartContractAddress = res.address;
+        console.log('Contract address: ' + smartContractAddress);
+
+        addSmartContractAddress(optionObj, smartContractAddress, optionSmartContract);
+        // TODO test the deployed contract
+        // testContract(res.address);
+      }
+      console.log("Error Deploying Contract!")
+    });
+    return smartContractAddress;
+
+  }
+
+
+  function addSmartContractAddress(optionObj, address, optionSmartContract) {
+    // add address of smart contract
+    const deployedContract = optionSmartContract.at(address);
+    // if options are mutable...
+    optionObj.setSmartContractAddress(address);
+  }
+
+
 
 }
+
+
 
 
 
