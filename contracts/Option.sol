f0bae6bb4960b749ecc59ccaf21ecc0362fb843f
Elliott Forde <eforde@mit.edu>
2018-04-25 14:13:23 -0400

    [Option] More clean up

--- contracts/Option.sol
+++ contracts/Option.sol
@@ -5,14 +5,12 @@ pragma solidity ^0.4.0;
 
 
 contract Option {
-  uint constant public numberETH = 1000000000000000000; // in wei
+  uint constant public underlyingAmount = 1000000000000000000; // 1 eth in wei
 
   address public optionBuyer;
   address public optionSeller;
 
-  uint public optionCreationTime;  // what is this needed for?
   bool public optionType;          // put = True, call = False.
-  // TODO(eforde): use enums for this?
   bool public optionCreatorType;   // buyer = True, seller = False.
 
   uint public strikePriceUSD;
@@ -20,6 +18,11 @@ contract Option {
   uint public cacellationTime;
   uint public maturityTime;
 
+  bool public isActive;
+
+  // BIG TODO(eforde): block.timestamp not secure...
+  // BIG TODO(eforde): block.timestamp not secure...
+  // BIG TODO(eforde): block.timestamp not secure...
 
   // Nuking enums to get stack depth under control? enum vs bool vs uint memory sizing?
   /* enum OptionType {call, put} */
@@ -27,68 +30,54 @@ contract Option {
   // inactive -> active
   // active   -> buyer can call exercise() if not expired
   // active   -> seller can call recalimFunds() if expired
-  enum State {inactive, active}
-
-  State public currentState;
 
   // events
 
   event LogTransferMade(address sender, address receiver, uint amount);
 
 
-  function Option(bool traderType, uint optionCreationTime,
-                  bool optionType, uint strikePriceUSD, 
-                  uint maturityTime, uint premiumAmount,
-                  uint cancellationTime) public {
+  function Option(bool optionType, uint strikePriceUSD, 
+                  uint maturityTime, uint cancellationTime,
+                  uint premiumAmount, bool traderType) public payable {
       require(optionType == False); // Only allow calls
+
+      // set optionBuyer/optionSeller based on optionCreatorType
+      if (traderType) {
+        require(msg.value == premiumAmount);
+        optionBuyer = msg.sender;
+      }
+      else {
+        require(msg.value == underlyingAmount);
+        optionSeller = msg.sender;
+      }
+
       optionCreatorType = traderType;
       optionType = optionType;
-      optionCreationTime = optionCreationTime;
       strikePriceUSD = strikePriceUSD;
       maturityTime = maturityTime;
       premiumAmount = premiumAmount;
       cancellationTime = cancellationTime;
 
-      // set optionBuyer/optionSeller based on optionCreatorType
-      if (traderType) {
-        optionBuyer = msg.sender;
-      }
-      else {
-        optionSeller = msg.sender;
-      }
-      currentState = State.inactive;
+      isActive = false;
   }
-
-
-  // call after Option constructed!
-  // TODO(eforde): we should have the deposit be on construction if possible
-  function initialDeposit(bool optionCreatorType) payable public {
-    if (optionCreatorType) { // optionBuyer
-      require(amount == premiumAmount);
-    }
-    else { // optionSeller
-      require(amount == numberETH);
-    }
-  }
-
 
   function activateContract(address addr, bool traderType) payable public{
-    require(currentState == States.inactive);
+    require(!isActive);
     require(traderType == !optionCreatorType);
     require(block.timestamp < cancellationTime); // TODO(eforde): otherwise cancel
     if (traderType) { // buyer
-      require(addr(this).balance == numberETH);
+      require(addr(this).balance == underlyingAmount);
       require(msg.amount == premiumAmount);
       require(optionSeller != 0);
       optionBuyer = msg.sender;
     }
     else { // seller
       require(addr(this).balance == premiumAmount);
-      require(msg.amount == numberETH);
+      require(msg.amount == underlyingAmount);
       require(optionBuyer != 0);
       optionSeller = msg.sender;
     }
-    currentState = State.active;
+    isActive = true;
     optionSeller.transfer(premiumAmount);
     // TODO(eforde): verify not susceptible to reentry
   }
@@ -100,13 +89,12 @@ contract Option {
     require(optionType == false); // TODO figure out put
     require(inTheMoney(currentETHPrice));
 
-    // less gass
-    require(numberETH <= address(this).balance); // should be equal!
+    require(underlyingAmount <= address(this).balance); // should be equal!
     
     // TODO(eforde): be careful about reentry here
 
     // suicide(optionBuyer);
-    // optionBuyer.transfer(numberETH);
+    // optionBuyer.transfer(underlyingAmount);
 
     // We don't want to send all funds to the buyer... just the difference
     // between (strikePrice - currentEthPrice) * eth_amount, and then the
@@ -115,15 +103,15 @@ contract Option {
   }
 
 
-  function cancel(address tradingAccountAddress) public returns (bool) {
-    require(currentState == States.inactive);
+  function cancel() public returns (bool) {
+    require(!isActive);
     require(msg.sender == optionCreatorAddress);
     suicide(msg.sender);
   }
 
 
-  function reclaimFunds(address tradingAccountAddress) public returns (bool) {
-    require(currentState == States.active);
+  function reclaimFunds() public returns (bool) {
+    require(isActive);
     require(msg.sender == optionSeller);
     require(block.timestamp > maturityTime);
     suicide(msg.sender);
