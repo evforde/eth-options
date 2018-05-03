62914411d3598ce0fd695b966d06bf183e2f329e
Elliott Forde <eforde@mit.edu>
2018-05-03 16:30:49 -0400

    [views, dashboard] Allow creation of new trading accounts from dashboard

---
+++ public/front-end-js/dashboard.js
@@ -0,0 +1,43 @@
+// can play around in js console with this
+var account;
+
+function setAccount(acc) {
+  account = acc;
+}
+
+$(document).ready(function() {
+
+const TradingAccount = web3.eth.contract(TradingAccountABI);
+
+$("#make-account").click(function() {
+  // TODO(eforde): contract instantiation should probably be done on server
+  // since we're gonna have to store mappings from address -> trading account
+  getMetamaskAccount(function(account) {
+    console.log("making trading account for ", account);
+    var contractInstance = TradingAccount.new({
+      data: TradingAccountBinary,
+      from: account,
+      gas: 1000000
+    }, function(err, data) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (data && data.address === undefined) {
+        console.log("waiting for someone to mine block...");
+        console.log("txn ", data.transactionHash, "follow progress at https://ropsten.etherscan.io/tx/" + data.transactionHash);
+        return;
+      }
+      if (data.address) {
+        console.log("successfully deployed contract at ", data.address);
+        console.log(data);
+        data.owner(function(err, data) {
+          console.log("owner of contract is ", err || data);
+        });
+        setAccount(data);
+      }
+    });
+  });
+});
+
+});
--- src/routes/views.js
+++ src/routes/views.js
@@ -1,6 +1,36 @@
 // dependencies
 const express = require("express");
-const router = express.Router();
+const router  = express.Router();
+const solc    = require("solc");
+const fs      = require("fs");
+
+//
+// setup
+//
+
+// const source = fs.readFileSync('./contracts/TradingAccount.sol', 'utf8');
+// const compiledContract = solc.compile(source, 1);
+// const abi = JSON.stringify(compiledContract.contracts['nameContract'].interface);
+// const bytecode = compiledContract.contracts['nameContract'].bytecode;
+
+var tradingAccountABI;
+var tradingAccountBinary;
+
+fs.readFile("./public/contracts/__contracts_TradingAccount_sol_TradingAccount.abi", "ascii", function(err, data) {
+  if (err)
+    throw err;  // make sure you've compiled the smart contracts!
+  tradingAccountABI = JSON.stringify(data);
+});
+
+fs.readFile("./public/contracts/__contracts_TradingAccount_sol_TradingAccount.bin", "ascii", function(err, data) {
+  if (err)
+    throw err;  // make sure you've compiled the smart contracts!
+  tradingAccountBinary = data;
+});
+
+//
+// views
+//
 
 router.get("/", function(req, res, next) {
   res.render("index");
@@ -11,7 +41,12 @@ router.get("/index", function(req, res, next) {
 });
 
 router.get("/dashboard", function(req, res, next) {
-  res.render("dashboard", { user: req.user });
+  console.log("ABI:", tradingAccountABI);
+  res.render("dashboard", {
+    user: req.user,
+    tradingAccountABI: tradingAccountABI,
+    tradingAccountBinary: tradingAccountBinary
+  });
 });
 
 router.get("/unfulfilled", function(req, res, next) {
