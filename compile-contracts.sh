9aa43e9a76b5f04943cb4dab3d7569a4cb5307c0
Elliott Forde <eforde@mit.edu>
2018-05-16 15:03:51 -0400

    [TradingAccount] Remove trading account contract

--- compile-contracts.sh
+++ compile-contracts.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
-echo "solcjs --abi --bin -o ./public/contracts/ ./contracts/TradingAccount.sol"
-solcjs --abi --bin -o ./public/contracts/ ./contracts/TradingAccount.sol || echo "Did you run `npm install -g solc`?"
-
 echo "solcjs --abi --bin -o ./public/contracts/ ./contracts/Option.sol"
 solcjs --abi --bin -o ./public/contracts/ ./contracts/Option.sol || echo "Did you run `npm install -g solc`?"
 
